82a87f86a3e75b5d83746dac28e6042ab58f520f
tyler-ingebrand <tyleringebrand@gmail.com>
2023-01-27 11:54:05 -0600

    Clustering exmaple

--- test_clustering.py
+++ test_clustering.py
@@ -2,12 +2,14 @@ from matplotlib import pyplot as plt
 from tslearn.generators import random_walks
 from tslearn.clustering import TimeSeriesKMeans
 
-# random data, create clusters
+# random data
 X = random_walks(n_ts=50, sz=32, d=1)
 print("Data: ", X.shape)
 print("\t", X.shape[0], " trajectories of length ", X.shape[1], " with ", X.shape[2], " dimensions")
-# km = TimeSeriesKMeans(n_clusters=3, metric="softdtw", max_iter=5, max_iter_barycenter=5, random_state=0).fit(X)
-km = TimeSeriesKMeans(n_clusters=3, metric="euclidean", max_iter=5, random_state=0).fit(X)
+
+# run clustering alg
+km = TimeSeriesKMeans(n_clusters=3, metric="softdtw", max_iter=5, max_iter_barycenter=5, random_state=0).fit(X)
+# km = TimeSeriesKMeans(n_clusters=3, metric="euclidean", max_iter=5, random_state=0).fit(X)
 print("Clusters: ", km.cluster_centers_.shape)
 print("\t", km.cluster_centers_.shape[0], " clusters of length ", km.cluster_centers_.shape[1], " with ", km.cluster_centers_.shape[2], " dimensions")
 print("Cluster labels: ", km.labels_)
