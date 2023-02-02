8c15d336b6b0a403a8dfa9d621d9d0de0786dc6b
Sophia Smith <smithsophia1688#gmail.com>
2023-02-02 14:09:17 -0600

    expeirment parameter changes

--- test.py
+++ test.py
@@ -11,7 +11,7 @@ from stable_baselines3.common.env_util import make_vec_env
 if __name__ == '__main__':
 
     # parameters ##
-    seed = 0
+    seed = 22
     number_envs = 8
     ###############
 
@@ -37,10 +37,11 @@ if __name__ == '__main__':
     # train
     model = CausalCuriousPPO("MlpPolicy", env,
                              episode_length=env.get_attr("_max_episode_length", [0])[0] + 1, # this env returns the index of last step, we want total number of steps
-                             episodes_per_update=2, verbose=1)
-    model.learn(total_timesteps=25000)
+                             episodes_per_update=8, verbose=1)
+    model.learn(total_timesteps=1000000)
 
 
+    print(" FINISHED LEARNING" )
     # show episode
     obs = env.reset()
     while True:
