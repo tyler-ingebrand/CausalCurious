41b0fa9cc6eb0e8de067497079b7e6bae9fb85fa
tyler-ingebrand <tyleringebrand@gmail.com>
2023-03-01 09:53:00 -0600

    timeit

--- example_clustering.py
+++ example_clustering.py
@@ -3,14 +3,18 @@ from matplotlib import pyplot as plt
 from tslearn.generators import random_walks
 from tslearn.clustering import TimeSeriesKMeans
 from tslearn.metrics import cdist_soft_dtw, soft_dtw_alignment
+import time
+
 
 # random data
-X = random_walks(n_ts=50, sz=32, d=1)
+X = random_walks(n_ts=64, sz=250, d=13)
 print("Data: ", X.shape)
 print("\t", X.shape[0], " trajectories of length ", X.shape[1], " with ", X.shape[2], " dimensions")
 
 # run clustering alg
-km = TimeSeriesKMeans(n_clusters=3, metric="softdtw", max_iter=5, max_iter_barycenter=5, random_state=0, n_jobs=-1).fit(X)
+start = time.time()
+km = TimeSeriesKMeans(n_clusters=3, metric="softdtw", max_iter=5, max_iter_barycenter=5, random_state=0, n_jobs=1).fit(X)
+print("Time = ", time.time() - start)
 # km = TimeSeriesKMeans(n_clusters=3, metric="euclidean", max_iter=5, random_state=0).fit(X)
 print("Clusters: ", km.cluster_centers_.shape)
 print("\t", km.cluster_centers_.shape[0], " clusters of length ", km.cluster_centers_.shape[1], " with ", km.cluster_centers_.shape[2], " dimensions")
